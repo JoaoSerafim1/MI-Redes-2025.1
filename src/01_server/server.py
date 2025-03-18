9c12c3d33d58bee2ea7a04353f2421072ffadf8a
JoaoSerafim1 <joaolucasacademico@gmail.com>
2025-03-18 15:17:15 -0300

    feature: client requesting ID (needs correction on both sides to work, but the base is ready)

---
+++ src/01_server/server.py
@@ -0,0 +1,105 @@
+import os
+import string
+import random
+import socket
+import json
+
+class Server():
+    
+    def __init__(self):
+
+        self.requestLog = {}
+        self.socket_receiver = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket_receiver.bind((socket.gethostbyname('charge_server'), 8001))
+
+    def getRandomID(type):
+
+        filePath = ""
+            
+        if (type == 0):
+            filePath = os.path.join("clientdata", "stations")
+        else:
+            filePath = os.path.join("clientdata", "vehicles")
+        
+        fileList = os.listdir(filePath)
+
+        lettersanddigits = string.ascii_uppercase + string.digits
+
+        while True:
+
+            randomID = ""
+
+            for count in range(0,24):
+                randomID += random.choice(lettersanddigits)
+            
+            if (("" + randomID + ".json") not in fileList):
+                
+                print("ID para o proximo cadastro de estacao de carga: " + randomID)
+                return randomID
+
+    def listenToRequest(self):
+        
+        self.socket_receiver.listen(2)
+        conn, add = self.socket_receiver.accept()
+        msg = conn.recv(1024)
+        
+        decodedBytes = msg.decode('UTF-8')
+        unserializedObj = json.loads(decodedBytes)
+
+        return (add, unserializedObj)
+
+    def registerChargeStation(requestParameters, stationAddress):
+        
+        stationID = requestParameters[0]
+
+        socket_sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        CLIENT = socket.gethostbyaddr(stationAddress)
+        socket_sender.connect((CLIENT, 8002))
+        
+        if ((len(stationInfo) >= 4) and stationID == randomID):
+            
+            stationFilePath = (os.path.join("clientdata", "stations", (stationID + ".json")))
+            
+            stationInfo = {}
+            stationInfo["coordinates"] = requestParameters[1]
+            stationInfo["available_spots"] = requestParameters[2]
+            stationInfo["unitary_price"] = requestParameters[3]
+            
+            stationFile = open(stationFilePath, "w")
+            json.dump(stationInfo, stationFile)
+            stationFile.close()
+
+            socket_sender.send(bytes('OK', 'UTF-8'))
+        
+        else:
+            socket_sender.send(bytes('ERR', 'UTF-8'))
+
+#Programa inicia aqui
+localServer = Server()
+
+randomID = localServer.getRandomID(0)
+
+while True:
+    
+    clientAddress, requestInfo = localServer.listenToRequest()
+
+    if (len(requestInfo >= 4)):
+        
+        clientID = requestInfo[0]
+        requestID = requestInfo[1]
+        requestName = requestInfo[2]
+        requestParameters = requestInfo[3]
+
+        if (((clientID in localServer.requestBook) == False) or (localServer.requestBook[clientID] != requestID)):
+            
+            localServer.requestBook[clientID] = requestID
+            
+            if (requestName == "rcs"):
+                localServer.registerChargeStation(requestParameters, clientAddress)
+    
+    else:
+
+        socket_sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        CLIENT = socket.gethostbyaddr(clientAddress)
+        socket_sender.connect((CLIENT, 8002))
+        socket_sender.send(bytes('ERR', 'UTF-8'))
--- src/03_vehicle/client.py
+++ src/03_vehicle/client.py
@@ -1,24 +1,39 @@
+import os
+import json
 import socket
 
 class User():
-    def __init__(self,user,vehicle,payment_method,host): 
+    
+    def __init__(self, ID, user, battery_level, vehicle, payment_method, host): 
+        
+        self.ID = ID
+        self.battery_level = battery_level
         self.user = user
         self.vehicle = vehicle
-        self.battery_level = 1.0
         self.payment_method = payment_method
         self.payment_history = {}
         
         self.socket_sender = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         hostname = socket.gethostbyname(host)
         self.socket_sender.connect((hostname,8001))
-        
+
+    def sendRequest(self): #envia requisicao simples (para fins de teste)
+        self.socket_sender.sendall((str.encode('ok')))
+
     def batteryCheck(self): #notifica se a bateria esta em estado critico
         if self.battery_level < 0.3:
             return 1
         return 0
     
-    def sendRequest(self): #envia requisicao simples (para fins de teste)
-        self.socket_sender.sendall((str.encode('ok')))   
+    def getNewID(self):
+        self.socket_sender.sendall((str.encode('getID')))
+        msg = self.socket_sender.recv(1024)
+
+        while (len(msg.decode()) != 24):
+            self.socket_sender.sendall((str.encode('getID')))
+            msg = self.socket_sender.recv(1024)
+
+        return msg.decode()
     
     def bookChargeSpot(self): #reserva posto
         self.socket_sender.sendall((str.encode('reservar posto')))
@@ -40,3 +55,49 @@ class User():
     
     def paymentCheck(self): #visualiza payment_history
         print(self.payment_history)
+    
+
+#Programa inicia aqui
+#Cria um objeto da classe User
+vehicle = User("", "", "", "", "", "charge_server")
+
+#Gera uma lista dos elementos presente no diretorio atual
+fileList = os.listdir()
+
+#Verifica se o arquivo de texto "ID.txt" esta presente, e caso nao esteja...
+if ("ID.txt" not in fileList):
+    
+    #...cria o arquivo e preenche com um novo ID
+    idFile = open("ID.txt", "w")
+    idFile.write(vehicle.getNewID())
+    idFile.close()
+
+#Verifica se o arquivo de texto "vehicle_data.json" esta presente, e caso nao esteja...
+if ("vehicle_data.json" not in fileList):
+    
+    #...cria um dicionario dos atributos dos veiculos e preenche com valores iniciais
+    dataTable = {}
+    dataTable["battery_level"] = "1.0"
+
+    #E tambem cria o arquivo e preenche com as informacoes contidas no dicionario acima (em string para evitar problemas com json)
+    dataFile = open("vehicle_data.json", "w")
+    json.dump(dataTable, dataFile)
+    dataFile.close()
+
+#Carrega as informacoes gravadas (ID)
+idFile = open("ID.txt", "r")
+vehicle_ID = idFile.read()
+idFile.close()
+
+#Carrega as informacoes gravadas (vehicle_data)
+dataFile = open("vehicle_data.json", "w")
+loadedTable = json.load(dataFile)
+dataFile.close()
+
+#Modifica as informacoes do objeto do veiculo
+vehicle.ID = vehicle_ID
+vehicle.battery_level = loadedTable["battery_level"]
+
+#Print de teste
+print(vehicle.ID)
+print(vehicle.battery_level)
